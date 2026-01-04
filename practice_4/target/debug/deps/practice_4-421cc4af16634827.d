6a8e12a8bb28b40ee129401276710258ab325d70
Mohammad Reza Joneidi Jafari <132068610+mrjoneidi@users.noreply.github.com>
2026-01-04 19:52:11 +0330

    Delete practice_4/target directory

--- practice_4/target/debug/deps/practice_4-421cc4af16634827.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ASUS\Desktop\Rust\rust-beginner\practice_4\target\debug\deps\practice_4-421cc4af16634827.d: src\main.rs
-
-C:\Users\ASUS\Desktop\Rust\rust-beginner\practice_4\target\debug\deps\practice_4-421cc4af16634827.exe: src\main.rs
-
-src\main.rs:
--- practice_4/target/debug/practice_4.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ASUS\Desktop\Rust\rust-beginner\practice_4\target\debug\practice_4.exe: C:\Users\ASUS\Desktop\Rust\rust-beginner\practice_4\src\main.rs
